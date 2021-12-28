5f454959d8c4687c97605fd7288bdad5c5f0c5ba
pixiedust07 <sharonshaji127001@gmail.com>
2021-12-28 23:15:09 +0530

    Fix test-expression in for loop

--- pattern_1.c
+++ pattern_1.c
@@ -13,7 +13,7 @@ int main() {
     int rowIndex, colIndex, limit;
     printf("Enter the limit : ");
     scanf("%d",&limit);
-    for(rowIndex = 0; rowIndex <= limit; rowIndex++) {
+    for(rowIndex = 0; rowIndex < limit; rowIndex++) {
         for (colIndex = 0; colIndex < limit; colIndex++) {
             printf(" * ");
         }
--- pattern_2.c
+++ pattern_2.c
@@ -14,10 +14,8 @@ int main()
     int rowIndex, colIndex, limit;
     printf("Enter the limit : ");
     scanf("%d", &limit);
-    for (rowIndex = 0; rowIndex <= limit; rowIndex++)
-    {
-        for (colIndex = 0; colIndex < rowIndex; colIndex++)
-        {
+    for (rowIndex = 0; rowIndex < limit; rowIndex++){
+        for (colIndex = 0; colIndex <= rowIndex; colIndex++) {
             printf(" * ");
         }
         printf("\n");
