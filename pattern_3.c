6fbaf44fe2e7f51db70d6df40773a81e28c00906
pixiedust07 <sharonshaji127001@gmail.com>
2021-12-28 23:21:08 +0530

    Pattern 3

---
+++ pattern_3.c
@@ -0,0 +1,25 @@
+/*
+pattern 1 :
+    * * * * *
+    * * * *
+    * * * 
+    * *
+    * 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int rowIndex, colIndex, limit;
+    printf("Enter the limit : ");
+    scanf("%d", &limit);
+    for (rowIndex = 0; rowIndex < limit; rowIndex++)
+    {
+        for (colIndex = limit; colIndex > rowIndex; colIndex--)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+}
