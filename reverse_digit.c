ce7b510f5059030c36955c65a5f5ddcfb7729b2e
pixiedust07 <sharonshaji127001@gmail.com>
2021-12-30 13:32:40 +0530

    Fix the case of lost trailing 0's and -ve numbers

--- reverse_digit.c
+++ reverse_digit.c
@@ -2,13 +2,27 @@
 
 int main()
 {
-    int number, rev_num = 0;
+    int number, rev_num = 0, zeroCount = 0, index;
     printf("Enter the number : ");
     scanf("%d", &number);
     printf("You Entered : %d\n", number);
+    if(number < 0) {
+        // convert when the input number is negative
+        number *= -1;
+    }
     while (number != 0) {
-        rev_num = (rev_num * 10) + (number % 10);
+        rev_num = 0 + (rev_num * 10) + (number % 10);
+        // get the number of trailing zeros
+        if(rev_num == 0) {
+            zeroCount++;
+        }
         number /= 10;
     }
-    printf("It's reverse : %d", rev_num);
+    // with zeroCount as the loop condition to append 0's before printing
+    printf("It's reverse : \n");
+    for ( index = 0; index < zeroCount; index++) {
+        printf("0");
+    }
+    printf("%d", rev_num);
+    
 }
