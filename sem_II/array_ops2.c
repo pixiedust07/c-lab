2f9f1eda13f040085cf6bf7d10b69242664143ea
pixiedust07 <sharonshaji127001@gmail.com>
2022-03-18 19:38:20 +0530

    array program-2

---
+++ sem_II/array_ops2.c
@@ -0,0 +1,25 @@
+// program to Read and print the sum of all elements in the array
+#include<stdio.h>
+
+void displaySum() {
+    int arr[10],index, size,sum = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d",&size);
+    printf("Enter the elements of the array: ");
+    for(index = 0; index < size;index++) {
+        scanf("%d",&arr[index]);
+    }
+
+    printf("The array elements are : ");
+    for(index = 0; index < size;index++) {
+        sum += arr[index];
+    }
+
+    printf("Sum = %d",sum);
+
+}
+
+void main() {
+
+    displaySum();
+}
